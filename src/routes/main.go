df130d0b5134d489298e25660adbc355b5a5ac68
Adil Kairbekov <79433274+qwertycamedy@users.noreply.github.com>
2024-02-13 23:18:55 +0600

    allowCors

--- src/routes/main.go
+++ src/routes/main.go
@@ -1,9 +1,14 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/qwertycamedy/140/back/src/middlewares"
+)
 
 func SetupRoutes() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(middlewares.AllowCors())
 
 	baseRouter := r.Group("/api/v1")
 
